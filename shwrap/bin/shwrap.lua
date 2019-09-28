6e47f2cb61253743c0cb0d51b8ad9c6dd4aabdce
Matt Master <mmaster@bitbix.com>
2019-09-28 18:18:15 +0200

    Small modifications and color changes

--- shwrap/bin/shwrap.lua
+++ shwrap/bin/shwrap.lua
@@ -1,11 +1,13 @@
+package.loaded['mmgui'] = nil
 local component = require("component")
+local term = require("term")
 local gpu = component.gpu
 local gui = require("mmgui")
 
 local prgName = "shwrap"
-local version = "0.1" .. "(MMGUI Lib " .. gui.Version() .. ")"
+local version = "0.1" .. " (MMGUI Lib " .. gui.Version() .. ")"
 
-function exitButtonCallback(guiID, id)
+function buttonExitCallback(guiID, id)
   local result = gui.getYesNo("", "Do you really want to exit?", "")
   if result == true then
     gui.exit()
@@ -15,12 +17,11 @@ end
 
 local screenWidth, screenHeight = gpu.getResolution()
 
-guiPanel = gui.newGui(screenWidth - 37, 2, 37, screenHeight - 1, false)
+guiPanel = gui.newGui(screenWidth - 36, 2, 37, screenHeight - 1, false, nil, 0x141512, 0xa0a0a0)
 
-exitButton = gui.newButton(guiStatus, "right", 1, "X", exitButtonCallback, 0x201010)
+buttonExit = gui.newButton(guiPanel, guiPanel.width - 1, 0, "X", buttonExitCallback, 0x201010, 0xff2222)
 
 gui.clearScreen()
-
 gui.setTop(prgName .. " " .. version)
 
 while true do
